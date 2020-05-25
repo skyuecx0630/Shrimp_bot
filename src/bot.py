c88006c702272e0a679d495f7c472bd3e8016cd9
hmoon630 <s19017@gsm.hs.kr>
2020-05-25 15:10:10 +0900

    feat: add meal parsing command

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,13 @@
 import discord
+from crawler import TimeCalc, MenuParser
+
+weekday_kor = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
 
 class ShrimpBot(discord.Client):
     def __init__ (self):
         self.prefix = '새우야'
+        self.color = 0xFF421A
+        self.meal_parser = MenuParser()
 
         super().__init__()
 
@@ -19,3 +24,23 @@ class ShrimpBot(discord.Client):
             command = message.content.lower()
             if command.startswith('새우야'):
                 await message.channel.send('안녕!')
+
+            if command.startswith('배고파') or command.startswith('qorhvk'):
+                await message.channel.trigger_typing()
+                
+                year, month, day, weekday, time = TimeCalc.get_next_time()
+
+                title = "%s년 %s월 %s일 %s %s밥" % (
+                    year, month, day, weekday_kor[weekday],
+                    ['아침', '점심', '저녁'][time]
+                )
+
+                menu = self.meal_parser.get_next_meal()
+
+                em = discord.Embed(
+                    title=title,
+                    description=menu,
+                    colour=self.color
+                )
+
+                await message.channel.send(embed=em)
---
+++ src/crawler.py
@@ -0,0 +1,42 @@
+from schapi import SchoolAPI
+from datetime import datetime, timedelta
+
+class TimeCalc:
+    @staticmethod
+    def get_next_time(now=datetime.now()):
+        """다음으로 조회할 급식의 시간을 반환합니다.
+
+        :param now: 조회할 시간
+        :type now: datetime
+        :return: (year, month, day, time)
+        :rtype: tuple
+        """
+        # 아침, 점심, 저녁 시간
+        meal_time = [510, 810, 1170]
+
+        for i in range(len(meal_time)):
+            if (now.hour * 60 + now.minute) < meal_time[i]:
+                return (now.year, now.month, now.day, now.weekday(), i)
+        
+        #금요일이면 다음날을 월요일로
+        now += timedelta(days=3 if now.weekday==4 else 1)
+    
+        return (now.year, now.month, now.day, now.weekday(), 0)
+
+
+class MenuParser:
+    def __init__(self):
+        self.api = SchoolAPI(SchoolAPI.Region.GWANGJU, 'F100000120')
+
+    def get_next_meal(self):
+        meal = ["breakfast", "lunch", "dinner"]
+
+        year, month, day, weekday, time = TimeCalc.get_next_time()
+        menu = getattr(self.api.get_monthly_menus(year, month)[day], meal[time])
+        result = "\n".join(["- %s" % item for item in menu])
+        
+        return result
+
+if __name__ == '__main__':
+    p = MenuParser()
+    print(p.get_next_meal())
