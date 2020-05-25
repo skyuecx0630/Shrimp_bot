15e1c64070fc88ad233e72ca94c5005b00b8ef98
hmoon630 <s19017@gsm.hs.kr>
2020-05-26 00:47:48 +0900

    refactor: command detection

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,14 @@
 import discord
 from crawler import TimeCalc, MenuParser
+from const import Constants
 
 weekday_kor = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
+
+def find_command(message):
+    for command, shorts in Constants.command.items():
+        if message in shorts:
+            return command
+
 
 class ShrimpBot(discord.Client):
     def __init__ (self):
@@ -11,36 +18,45 @@ class ShrimpBot(discord.Client):
 
         super().__init__()
 
+
     async def on_ready(self):
         activity = discord.Activity(name='명령어: 새우야', type=discord.ActivityType.playing)
         await self.change_presence(activity=activity)
         print('Bot Started!')
     
+
     async def on_message(self, message):
         await self.wait_until_ready()
 
         if not message.author.bot:
-
             command = message.content.lower()
-            if command.startswith('새우야'):
-                await message.channel.send('안녕!')
 
-            if command.startswith('배고파') or command.startswith('qorhvk'):
-                await message.channel.trigger_typing()
+            func = getattr(self, "command_%s" % find_command(command.split()[0]), None)
+
+            if func:
+                await func(message)
+
+
+    async def command_ping(self, message):
+        await message.channel.send('안녕!')
+
+
+    async def command_hungry(self, message):
+        await message.channel.trigger_typing()
                 
-                year, month, day, weekday, time = TimeCalc.get_next_time()
+        year, month, day, weekday, time = TimeCalc.get_next_time()
 
-                title = "%s년 %s월 %s일 %s %s밥" % (
-                    year, month, day, weekday_kor[weekday],
-                    ['아침', '점심', '저녁'][time]
-                )
+        title = "%s년 %s월 %s일 %s %s밥" % (
+            year, month, day, weekday_kor[weekday],
+            ['아침', '점심', '저녁'][time]
+        )
 
-                menu = self.meal_parser.get_next_meal()
+        menu = self.meal_parser.get_next_meal()
 
-                em = discord.Embed(
-                    title=title,
-                    description=menu,
-                    colour=self.color
-                )
+        em = discord.Embed(
+            title=title,
+            description=menu,
+            colour=self.color
+        )
 
-                await message.channel.send(embed=em)
+        await message.channel.send(embed=em)
---
+++ src/const.py
@@ -0,0 +1,5 @@
+class Constants:
+    command = {
+        'ping' : ['ping', '새우야'],
+        'hungry' : ['배고파', 'qorhvk', 'hungry'],
+    }
