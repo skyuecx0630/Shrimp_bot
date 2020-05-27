c63db9cd484765377310b61eb20808ff47d56ecf
hmoon630 <s19017@gsm.hs.kr>
2020-05-27 20:12:32 +0900

    refactor: token imort

--- src/const/__init__.py
+++ src/const/__init__.py
@@ -1,2 +1,3 @@
 from .const import Constants
 from .docs import Docs
+from .settings import Token
--- src/run.py
+++ src/run.py
@@ -1,20 +1,8 @@
-import configparser
-from os.path import dirname, exists, join
-
 from bot import ShrimpBot
 from utils import Timer
-
-CONFIG_FILE = join('..', 'config', 'config.ini')
-
-if exists(CONFIG_FILE):
-    parser = configparser.ConfigParser()
-    parser.read(CONFIG_FILE)
-else:
-    raise FileNotFoundError('%s doesn\'t exists' % CONFIG_FILE)
-
-token = parser.get("Default", "token")
+from const import Token
 
 timer = Timer()
-ShrimpBot().run(token)
+ShrimpBot().run(Token.token)
 
 print("Run time - %dh : %dm : %ds" % timer.end())
