925e89432bf6458807824ca767f7dc38e79d4318
hmoon630 <s19017@gsm.hs.kr>
2020-07-29 20:30:44 +0900

    feat: change to make session on every transaction

--- src/handlers/db_manager.py
+++ src/handlers/db_manager.py
@@ -1,8 +1,6 @@
-import configparser
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from os.path import join, exists
+
 from models import Base, Custom_commands
 
 from const import Settings
@@ -10,26 +8,39 @@ from const import Settings
 
 class DBManager:
     def __init__(self):
-        engine = create_engine(Settings.database)
+        self.engine = create_engine(Settings.database)
 
-        Base.metadata.create_all(engine)
-
-        session = sessionmaker()
-        session.configure(bind=engine)
-        self.session = session()
+        Base.metadata.create_all(self.engine)
 
     def insert_row(self, data):
-        self.session.add(data)
-        self.session.commit()
+        with Session(self.engine) as session:
+            session.add(data)
+            session.commit()
 
     def search_row(self, table, column, data):
         key = getattr(table, column)
-        query = self.session.query(table).filter(key == (data))
+        with Session(self.engine) as session:
+            query = session.query(table).filter(key == (data))
         return query.all()
 
     def delete_row(self, data):
-        self.session.delete(data)
-        self.session.commit()
+        with Session(self.engine) as session:
+            session.delete(data)
+            session.commit()
+
+
+class Session:
+    def __init__(self, engine):
+        self.engine = engine
+        self.session = None
+
+    def __enter__(self):
+        self.session = sessionmaker(bind=self.engine)()
+
+        return self.session
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
 
 
 if __name__ == "__main__":
